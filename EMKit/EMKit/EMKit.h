222a1f99a4f6c893869aadb98b555b69bfb4eb51
Pedro Anibarro <pedroanibarro3@gmail.com>
2018-06-18 14:26:29 -0400

    Added CocoaPod

---
+++ EMKit/EMKit/EMKit.h
@@ -0,0 +1,19 @@
+//
+//  EMKit.h
+//  EMKit
+//
+//  Created by Pedro Anibarro on 6/18/18.
+//  Copyright © 2018 ErraticMinds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EMKit.
+FOUNDATION_EXPORT double EMKitVersionNumber;
+
+//! Project version string for EMKit.
+FOUNDATION_EXPORT const unsigned char EMKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EMKit/PublicHeader.h>
+
+
---
+++ Extensions/TimeInterval+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  URLRequest+Extension.swift
+//
+//
+//  Created by Pedro Anibarro on 6/18/18.
+//
+
+import Foundation
+
+extension TimeInterval {
+
+    static let now: TimeInterval = 0.0
+
+}
